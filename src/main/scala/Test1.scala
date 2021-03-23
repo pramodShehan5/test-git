cb6e9fab4fbc67d22662d296ef474c8ddd9c2e49
pramods <pramod.shehan@pagero.com>
2021-03-23 22:05:25 +0530

    Init

---
+++ src/main/scala/Test1.scala
@@ -0,0 +1,14 @@
+object Test1 {
+  def main(args: Array[String]): Unit = {
+    val days = Iterable("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
+    val aa = days.toVector.sorted.zipWithIndex
+      aa.foreach {
+      case(day, count) => println(s"$count is $day")
+    }
+    val bb = Option("messageData").filterNot(_.isEmpty).getOrElse("Unknown Exception")
+    println(bb)
+    for {
+      (srcBaseDir, i) <- days.toVector.sorted.zipWithIndex
+    } yield println(s"aaa $srcBaseDir and $i")
+  }
+}
